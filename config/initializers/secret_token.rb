124576cba2577743d673d0badde71f9d6d88a8f9
Shamick Bindra <shamick.bindra@gmail.com>
2012-04-29 19:21:55 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ibetyou::Application.config.secret_token = '6398839c9e946181cc61f8c1ed6a0b16093fd75a2b8103c5ccce8fb081000dafd1bc0ab371fd98a01ada9abe34b5ebc35cf2cd04d6058a35e57ce5546e980de5'
